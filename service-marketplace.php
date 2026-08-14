da8d53361ae45381930dbe985a8f873cdb204fdf
Sphamandla Nkosi <nkosispha123@gmail.com>
2026-08-14 21:26:49 +0200

    initial service marketplace plugin

---
+++ service-marketplace.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Plugin Name:       Service Marketplace
+ * Plugin URI:        https://github.com/your-username/wordpress-service-marketplace
+ * Description:       A custom multi-business service marketplace for WordPress.
+ * Version:           1.0.0
+ * Author:            Your Name
+ * Text Domain:       service-marketplace
+ */
+
+// Exit if accessed directly
+if (!defined('ABSPATH')) {
+    exit('No direct script access allowed');
+}
