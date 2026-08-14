47b5f3914fa8cbfb5920f923ab10e094d71e79b2
Sphamandla Nkosi <nkosispha123@gmail.com>
2026-08-14 22:16:53 +0200

    register custom busines, and customer roles

--- service-marketplace.php
+++ service-marketplace.php
@@ -12,3 +12,35 @@
 if (!defined('ABSPATH')) {
     exit('No direct script access allowed');
 }
+
+/**
+ * Register custom roles on plugin activation.
+ */
+function sm_register_custom_roles() {
+    // Bussiness Role
+    add_role(
+        'business',
+        'Bussiness Owner',
+        array(
+            'read' => true,
+            'upload_files' => true,
+            'edit_posts' => true,
+        )
+    );
+
+    // Customer Role
+    add_role(
+        'customer',
+        'Customer',
+        array(
+            'read' => true,
+            'upload_files' => false,
+            'edit_posts' => false,
+        )
+    );
+}
+
+register_activation_hook(
+    __FILE__,
+    'sm_register_custom_roles'
+);
